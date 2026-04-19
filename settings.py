04ffb0a6e6359bec204834d692bc4e0235a3bb7f
StefashovaLiudmila <l.stefashova@yandex.ru>
2026-04-19 09:41:49 +0000

    ✅ Задание 1 и 2: kafkacat -L и отправка сообщений в Kafka

--- settings.py
+++ settings.py
@@ -1,2 +1,2 @@
-USER_HOST = 'xx.xx.xx.xx' # укажите свой хост
-TOPIC_NAME = 'student.topic.cohort<номер когорты>.<username>' # укажите название топика student.topic.cohort<номер когорты>.<username>
+USER_HOST = '111.88.246.40' # укажите свой хост
+TOPIC_NAME = 'student.topic.cohort45.s24270523' # укажите название топика student.topic.cohort<номер когорты>.<username>
--- Тема 3. Настройка потока данных/4. Изучение данных в Kafka со Spark/Задание 2/realization.py
+++ Тема 3. Настройка потока данных/4. Изучение данных в Kafka со Spark/Задание 2/realization.py
@@ -2,36 +2,51 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as f, DataFrame
 from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, StringType
 
-# необходимая библиотека с идентификатором в maven
-# вы можете использовать ее с помощью метода .config и опции "spark.jars.packages"
-kafka_lib_id =
-        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
+kafka_lib_id = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
 
-# настройки security для кафки
-# вы можете использовать из с помощью метода .options(**kafka_security_options)
 kafka_security_options = {
     'kafka.security.protocol': 'SASL_SSL',
     'kafka.sasl.mechanism': 'SCRAM-SHA-512',
-    'kafka.sasl.jaas.config': 'org.apache.kafka.common.security.scram.ScramLoginModule required username=\"de-student\" password=\"ltcneltyn\";',
+    'kafka.sasl.jaas.config': 'org.apache.kafka.common.security.scram.ScramLoginModule required username="de-student" password="ltcneltyn";',
 }
 
 def spark_init() -> SparkSession:
-    pass
+    return SparkSession.builder \
+        .appName("kafka_task") \
+        .config("spark.jars.packages", kafka_lib_id) \
+        .getOrCreate()
 
 
 def load_df(spark: SparkSession) -> DataFrame:
-    pass
+    kafka_bootstrap = "rc1b-2erh7b35n4j4v869.mdb.yandexcloud.net:9091"
+    topic_name = "student.topic.cohort41.mark9891"  # ← ЗАМЕНИТЕ НА СВОЙ!
+    
+    return spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", kafka_bootstrap) \
+        .option("subscribe", topic_name) \
+        .options(**kafka_security_options) \
+        .option("ssl.ca.location", "/app/certs/CA.pem") \
+        .option("startingOffsets", "earliest") \
+        .load()
 
 
 def transform(df: DataFrame) -> DataFrame:
-    pass
+    schema = StructType([
+        StructField("id", IntegerType(), True),
+        StructField("album", StringType(), True),
+        StructField("artist", StringType(), True),
+        StructField("sold", StringType(), True)
+    ])
+    
+    return df.selectExpr("CAST(value AS STRING) as json_value") \
+        .select(f.from_json(f.col("json_value"), schema).alias("data")) \
+        .select("data.*")
 
 
 spark = spark_init()
-
 source_df = load_df(spark)
 df = transform(source_df)
-
 
 df.printSchema()
 df.show(truncate=False)
